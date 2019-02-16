5fc456089fb58d9dd589d5cf6f69a173eab8e456
Adrien Desmet <adrien.desmet@hotmail.fr>
2019-02-16 12:55:48 +0100

    Suppression de code mort

--- webpack.config.js
+++ webpack.config.js
@@ -20,12 +20,13 @@ encore
 
     .addEntry('app', './src/Assets/js/app.js')
     .enableVueLoader()
+
     .addLoader({
         test: /\.scss$/,
         use: [
-            "style-loader", // creates style nodes from JS strings
-            "css-loader", // translates CSS into CommonJS
-            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            "style-loader",
+            "css-loader",
+            "sass-loader"
         ]
     })
 
@@ -33,36 +34,3 @@ encore
     .cleanupOutputBeforeBuild();
 
 module.exports = encore.getWebpackConfig();
-
-//Configuration Webpack, au cas ou...
-
-// module.exports = {
-//     entry: './src/Assets/js/app.js',
-//     output: {
-//         path: path.resolve(__dirname, 'public/build/js'),
-//         filename: 'app.js'
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.css$/,
-//                 use: [
-//                     'style-loader',
-//                     'css-loader'
-//                 ]
-//             },
-//             {
-//                 test: /\.vue$/,
-//                 loader: 'vue-loader'
-//             }
-//         ]
-//     },
-//     plugins: [
-//         new webpack.ProvidePlugin({
-//             $: "jquery",
-//             jQuery: "jquery",
-//             jquery: "jquery",
-//         }),
-//         new VueLoaderPlugin()
-//     ]
-// };
