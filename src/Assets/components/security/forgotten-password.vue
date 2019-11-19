b77f5c29359a11f7c5db1f2cb2118a7ba00b4d13
Adrien Desmet <adrien.desmet@hotmail.fr>
2019-11-19 20:30:07 +0100

    Correction d'un problème de responsive + message d'erreur avec une alert

--- src/Assets/components/security/forgotten-password.vue
+++ src/Assets/components/security/forgotten-password.vue
@@ -6,13 +6,13 @@
                 <button class="uk-modal-close-default" type="button" uk-close></button>
                 <h3 class="uk-card-title">Mot de passe oublié</h3>
                 <p>Un mail avec la procédure de la reinitilisation de votre mot de passe va vous être envoyé. Pour cela,
-                    saisissez votre adrese mail.</p>
+                    saisissez votre adrese mail de votre compte</p>
 
                 <form action="">
                     <div class="uk-margin">
                         <div class="uk-inline uk-width-1">
                             <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: mail"></span>
-                            <input class="uk-input" type="text" id="username" name="_username"
+                            <input class="uk-input" type="text" id="username_forgotten" name="_username"
                                    placeholder="Adresse email">
                         </div>
                     </div>
